32007da52f8ad7f103cc1708fe48e834475c8619
PandaGPI <pirograg@gmail.com>
2023-11-08 20:46:31 +0300

    TASK002: 1.Работа над ошибками

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,18 +6,14 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld bean1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
         HelloWorld bean2 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        Cat beanCat = (Cat) applicationContext.getBean("cat");
+        Cat beanCat1 = (Cat) applicationContext.getBean("cat");
         Cat beanCat2 = (Cat) applicationContext.getBean("cat");
 
-        System.out.println(beanCat.getName());
-        System.out.println(beanCat2.getName());
-        System.out.println(bean.getMessage());
-        System.out.println(bean2.getMessage());
-        System.out.println(bean == bean2);
-        System.out.println(beanCat == beanCat2);
+        System.out.println(bean1 == bean2);
+        System.out.println(beanCat1 == beanCat2);
     }
 }
